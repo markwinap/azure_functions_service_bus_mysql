19a7570a992f0e84314d1912d2ee5295408a88a9
marmarti <markwinap@gmail.com>
2022-04-22 20:31:30 -0500

    Update To Insert Data in Mysql

--- DBInsertServiceBusQueueTrigger.cs
+++ DBInsertServiceBusQueueTrigger.cs
@@ -1,16 +1,64 @@
 using System;
+using System.Text.Json;
 using Microsoft.Azure.WebJobs;
 using Microsoft.Azure.WebJobs.Host;
 using Microsoft.Extensions.Logging;
+using MySql.Data.MySqlClient;
 
 namespace POCAzureFunctions
 {
     public static class DBInsertServiceBusQueueTrigger
     {
-        [FunctionName("DBInsertServiceBusQueueTrigger")]
-        public static void Run([ServiceBusTrigger("myqueue", Connection = "pocazurefunction_SERVICEBUS")]string myQueueItem, ILogger log)
+        
+        public class User
         {
-            log.LogInformation($"C# ServiceBus queue trigger function processed message: {myQueueItem}");
+            public string FullName { get; set; }
+            public string Age { get; set; }
+        }
+
+        [FunctionName("DBInsertServiceBusQueueTrigger")]
+        public static void Run([ServiceBusTrigger("myqueue", Connection = "pocazurefunction_SERVICEBUS")]string jsonString, ILogger log)
+        {
+            log.LogInformation($"C# ServiceBus queue trigger function processed message: {jsonString}");
+            try{
+
+                // MYSQL Client
+                string connectionString = @"server=localhost;userid=root;password=test;database=demo";
+                using var mysqlClient = new MySqlConnection(connectionString);
+                // Parse JSON String
+                var users = JsonSerializer.Deserialize<User[]>(jsonString);
+
+                //Read Users Arr
+                foreach (var user in users)
+                {
+                    var Uid = Guid.NewGuid().ToString();
+                    var FullName = user.FullName;
+                    var Age = user.Age;
+                    log.LogInformation($"User: {Uid} - {FullName} - {Age}");
+                    MySqlCommand cmd = new MySqlCommand($"CALL postUser('{Uid}', '{FullName}', {Age})");
+                    cmd.Connection = mysqlClient;
+                    mysqlClient.Open();
+                    MySqlDataReader reader = cmd.ExecuteReader();
+                    if (reader.HasRows)
+                    {
+                        while (reader.Read())
+                        {
+                            var Id = reader.GetGuid(0);
+                            log.LogInformation($"Id: {Id}");
+                        }
+                    }
+                    else
+                    {
+                        throw new InvalidOperationException("Unable to insert user.");
+                    }
+                    reader.Close();
+                    cmd.Connection.Close();
+                }
+            }
+            catch(Exception ex)
+            {
+                log.LogError(ex.Message);
+            }
         }
     }
 }
--- README.md
+++ README.md
@@ -4,9 +4,11 @@
 https://docs.microsoft.com/en-us/azure/azure-functions/functions-bindings-service-bus-trigger?tabs=in-process%2Cextensionv5&pivots=programming-language-csharp
 
 ## Requirements
-- Storage Emulator https://azure.microsoft.com/en-us
-- 
-- 
+- Storage Emulator - https://docs.microsoft.com/en-us/azure/storage/common/storage-use-emulator#get-the-storage-emulator
+
+## Packages
+dotnet add package MySql.Data
+
 ## Start Local Storage Emulator
 
 Azure Storage Emulator
@@ -16,10 +18,51 @@ https://docs.microsoft.com/en-us/azure/storage/common/storage-use-emulator#start
 
 ### Commands
 ```sh
-    AzureStorageEmulator.exe init            : Initialize the emulator database and configuration.
-    AzureStorageEmulator.exe start           : Start the emulator.
-    AzureStorageEmulator.exe stop            : Stop the emulator.
-    AzureStorageEmulator.exe status          : Get current emulator status.
-    AzureStorageEmulator.exe clear           : Delete all data in the emulator.
-    AzureStorageEmulator.exe help [command]  : Show general or command-specific help.
+AzureStorageEmulator.exe init            : Initialize the emulator database and configuration.
+AzureStorageEmulator.exe start           : Start the emulator.
+AzureStorageEmulator.exe stop            : Stop the emulator.
+AzureStorageEmulator.exe status          : Get current emulator status.
+AzureStorageEmulator.exe clear           : Delete all data in the emulator.
+AzureStorageEmulator.exe help [command]  : Show general or command-specific help.
+```
+
+## MySQl
+
+### Docker Image
+```sh
+docker run --name some-mysql -p 3306:3306 -v ./db:/var/lib/mysql -e MYSQL_ROOT_PASSWORD=test -d mysql:latest
+```
+
+### Create Database and tables
+```sql
+CREATE DATABASE demo;
+```
+### Create Users Table
+```sql
+USE demo;
+CREATE TABLE IF NOT EXISTS Users (
+    UserId BINARY(16) PRIMARY KEY,
+    FullName VARCHAR(150),
+    Age INT,
+    Created DATETIME
+);
+```
+
+### Create Store Procedure
+```sql
+USE demo;
+delimiter //
+CREATE PROCEDURE postUser (
+    IN _UserId VARCHAR(40),
+    IN _FullName VARCHAR(150),
+    IN _Age INT
+) BEGIN
+INSERT INTO Users (UserId, FullName, Age, Created) VALUES(UUID_TO_BIN(_UserId), _FullName, _Age, now());
+SELECT BIN_TO_UUID(UserId) AS UserId, FullName, Age, Created FROM Users WHERE UserId = UUID_TO_BIN(_UserId);
+END//
+```
+
+### Insert Data Example
+```sql
+CALL postUser("0f898832-f191-428c-b24b-cb56f88208b1", "Marco", 35);
 ```
